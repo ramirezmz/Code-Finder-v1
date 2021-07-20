4c452dda561ee5a5334f7a166ba3d55cc170c377
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2021-07-20 01:12:51 -0300

    version 1

---
+++ src/components/SearchBar.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./SearchBar.css";
+import SearchIcon from "@material-ui/icons/Search";
+import CloseIcon from "@material-ui/icons/Close";
+import RadioButton from "./RadioButton"
+
+function SearchBar({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = data.filter((value) => {
+      return value.Descricao.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
+  return (
+    <div className="search">
+      {/* <RadioButton /> */}
+      <h1>CODE FINDER</h1>
+      <div className="searchInputs">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
+        <div className="searchIcon">{filteredData.length === 0 ? (<SearchIcon />) : (<CloseIcon id="clearBtn" onClick={clearInput} />)}
+        </div>
+      </div>
+      {filteredData.length != 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 20).map((value, key) => {
+            return (
+              <a className="dataItem" >
+               <p>{value.Descricao}</p>
+                <p>{value.Codigo}</p>
+              </a>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SearchBar;
